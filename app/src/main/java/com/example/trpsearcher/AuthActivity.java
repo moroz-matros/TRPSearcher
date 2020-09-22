618d5044118a839e9e3ecbeb4a3737a25c183ad0
moroz-matros <morozovaanastasiaa2001@gmail.com>
2020-09-22 20:12:06 +0300

    Work on registration view

--- app/src/main/java/com/example/trpsearcher/AuthActivity.java
+++ app/src/main/java/com/example/trpsearcher/AuthActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private Button signIn;
+    private TextView register;
+    private EditText login;
+    private EditText password;
 
     public void onClickRegister(View view){
 
@@ -14,6 +21,14 @@ public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_auth);
+
+        signIn = findViewById(R.id.login_login);
+        register = findViewById(R.id.login_registration);
+        login = findViewById(R.id.login_edit);
+        password = findViewById(R.id.login_password);
+
+
+
     }
 }
