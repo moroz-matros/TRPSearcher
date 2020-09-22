f4dcbe22e86a6f2fd511187fc83bd36224d49f7f
moroz-matros <morozovaanastasiaa2001@gmail.com>
2020-09-22 19:28:05 +0300

    Initial

---
+++ app/src/main/java/com/example/trpsearcher/AuthActivity.java
@@ -0,0 +1,19 @@
+package com.example.trpsearcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AuthActivity extends AppCompatActivity {
+
+
+    public void onClickRegister(View view){
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
